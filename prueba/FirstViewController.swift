a35275b1d75520a7b1965e9a6d2c1c8cdf978fcc
jorgejuan007 <jjlafuente@sepg.meh.es>
2015-11-25 23:48:07 +0100

    Initial Commit

---
+++ prueba/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  prueba
+//
+//  Created by JorgeJuan on 25/11/15.
+//  Copyright © 2015 Jorge Juan Lafuente Carrasco. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ prueba/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  prueba
+//
+//  Created by JorgeJuan on 25/11/15.
+//  Copyright © 2015 Jorge Juan Lafuente Carrasco. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
